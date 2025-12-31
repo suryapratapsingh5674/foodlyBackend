e9d502f702efc6f30fdaf8a6fbaa80c8e6a9a0c3
surya pratap singh tomar <87597833+suryapratapsingh5674@users.noreply.github.com>
2025-12-31 13:40:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://foodlybackend-689k.onrender.com",
     credentials: true
 }))
 
